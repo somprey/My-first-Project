eaa72f654be6777d48bb941186a3828439c95ab1
Sylvester <somprey@gmail.com>
2020-10-07 01:52:46 +0000

    New Component Added

--- clockapp/src/App.js
+++ clockapp/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import NewApp from './NewApp';
 
+import NewComponent from './NewComponent';
 
 
 
@@ -9,7 +10,7 @@ function App(){
   return(
     <div className="vid" autoplay loop muted>
       <NewApp/>
-      
+      <NewComponent/>
     </div>
   );
 
---
+++ clockapp/src/NewComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function NewComponent (){
+return (
+    <div>
+        <h1> This is an Introduction to React </h1>
+        <p> Welcome to the very first module of the course and brace yourselves up for even more challenging times ahead!</p>
+        
+        <table style= "width:100%">
+            <tr>
+                <th>Java</th>
+                <th>TavaScript</th>
+                <th>HTML</th>
+            </tr>
+
+            <tr>
+                <td>Alfred</td>
+                <td>Mike</td>
+                <td>Liana</td>
+            </tr>
+
+        </table>
+        </div>
+
+        );
+        }
+
+export default NewComponent; 
